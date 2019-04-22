538b8283692e0ed72ad05a1a267ca6a7774c992f
Bibek <bibekmanshrestha@gmail.com>
2019-04-21 21:27:09 -0400

    initial commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.app')
+
+@section('page_title', 'Home')
+
+
+@section('content')
+
+<div id="home_container" class="d-flex flex-column justify-content-center">
+    <h1 class="display-4 text-center">Home for all DOTA2 lovers</h1>
+    <form role="search" id="home_search_form" class="d-flex justify-content-center">
+        <div class="d-flex flex-row col-8 mt-5 mb-3" id="form_container">
+            <input class="flex-grow-1 search-query" type="search" name="q"
+                placeholder="Search for players, teams or heroes" aria-label="Search for players, teams or heroes">
+            <button class="search_btn">
+                <img src="/images/search-icon.png" alt="" srcset="">
+            </button>
+        </div>
+    </form>
+    <div class="text-center">
+        Or browse categories from the left navigation panel.
+    </div>
+</div>
+
+@endsection
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>@yield('page_title') - DotaStats</title>
+  <link rel="stylesheet" href="\css\app.css">
+</head>
+<body class="d-flex flex-row">
+  <div class="d-flex flex-column" id="sidebar">
+    @section('sidebar')
+        <div id="user_container" class="d-flex flex-column align-items-center">
+          <img src="/images/smiley-face.png" alt="" srcset="">
+          <a href="#">Sign in using Steam</a>
+        </div>
+        <nav class="nav flex-column">
+          <a class="nav-link active" href="/">Home</a>
+          <a class="nav-link" href="/heroes">Heroes</a>
+          <a class="nav-link" href="/teams">Teams</a>
+          <a class="nav-link" href="/tournaments">Tournaments</a>
+        </nav>
+    @show
+  </div>
+
+  <div class="container main_container flex-grow-1">
+    @yield('content')
+  </div>
+
+</body>
+</html>
