ae7ad6a3961b59dbb42b8963796446781b76a3f1
Bibek <bibekmanshrestha@gmail.com>
2019-04-24 23:55:57 -0400

    steam login functions .. very basic with no error

--- app/Http/Controllers/Auth/SteamLoginController.php
+++ app/Http/Controllers/Auth/SteamLoginController.php
@@ -7,32 +7,17 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
 use kanalumaddela\LaravelSteamLogin\Http\Controllers\AbstractSteamLoginController;
-use kanalumaddela\LaravelSteamLogin\Http\Controllers\SteamLoginHandlerController;
 use kanalumaddela\LaravelSteamLogin\SteamUser;
 
-class SteamLoginController extends SteamLoginHandlerController
+class SteamLoginController extends AbstractSteamLoginController
 {
     /**
      * {@inheritdoc}
      */
     
-    public function authenticate(Request $request, SteamUser $steamUser)
+    public function authenticated(Request $request, SteamUser $steamUser)
     {
-      // find user by steam account id
-      $user = StmUser::where('steam_account_id', $steamUser->accountId);
-
-      // if the user doesn't exist, create them
-      if (!$user) {
-          $steamUser->getUserInfo(); // retrieve and set user info pulled from steam
-
-          $user = User::create([
-              'name' => $steamUser->name, // personaname
-              'steam_account_id' => $steamUser->accountId,
-          ]);
-      }
-
-      // login the user using the Auth facade
-      // the extended Abstract controller redirects the user back to the page they were on since there was no return
-      Auth::login($user);
+      $steamUser->getUserInfo();
+        dd($steamUser);
     }
 }
